62aca9d6aa25e2fc7a02265f08d5bc77f7f766cf
Ana-Karoline-Tech <anna.karolinawork@gmail.com>
2025-02-15 23:18:39 -0300

    Primeiro commit do pojeto Spotify referente Jornada Full Stack - Aula 4
    Hashtag Programação

---
+++ Aula 1/Gabarito da Aula 1/src/assets/App2.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+// rafce
+
+// Arrow Fnction
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de Componente
+// PascalCase
+
+// Nomeação de Variável, Função...
+//camelCaseCor
+
+// export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+
+// Self closingtag
+// <Header></Header>
+// <Header/>
+
+// Element.style {
+//  background-color: antiquewhite;
+// passing: 20pageXOffset;
+//  margin: 20px;
+//  border: solid 5px black;
+//  /*box-sizing: <content-box></content-box>*/
+//  with:500px;
+// }
+
+// Nomeação de classes em CSS
+// Metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento--modificador
+// header__link
+// header__link--small
+
+// Tag vazia em React se chama Fragment ou Fragmento
